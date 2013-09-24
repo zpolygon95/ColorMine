b29d3b4594bdd6fd940c6f6afa892b468bb1c14a
THEjoezack <me@joezack.com>
2013-09-24 19:38:03 -0400

    DE2000 compiling, woo!

---
+++ ColorMine.Test/ColorSpaces/Comparisons/CieDe2000Test.cs
@@ -0,0 +1,42 @@
+﻿using ColorMine.ColorSpaces;
+using ColorMine.ColorSpaces.Comparisons;
+using ColorMine.ColorSpaces.Conversions.Utility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ColorMine.Test.ColorSpaces.Comparisons
+{
+    public class CieDe2000Test
+    {
+        [TestClass]
+        public class Compare
+        {
+            private void ReturnsExpectedValueForKnownInput(double expectedValue, IColorSpace a, IColorSpace b)
+            {
+                var target = new CieDe2000Comparison();
+                var actualValue = a.Compare(b, target);
+                Assert.IsTrue(expectedValue.BasicallyEqualTo(actualValue), expectedValue + " != " + actualValue);
+            }
+
+            [TestMethod]
+            public void ReturnsKnownValueForRedAndMaroon2()
+            {
+                // Todo, should be mocking!!
+                var a = new Lab
+                {
+                    L = 24.8290,
+                    A = 60.0930,
+                    B = 38.1800
+                };
+
+                var b = new Lab
+                {
+                    L = 53.2300,
+                    A = 80.1090,
+                    B = 67.2200
+                };
+
+                ReturnsExpectedValueForKnownInput(2, a, b);
+            }
+        }
+    }
+}
