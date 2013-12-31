619d01e6ce0f5d270b36b1a45ecd08ab857db28a
THEjoezack <me@joezack.com>
2013-12-30 21:37:01 -0500

    Just updating for 4.5.1

--- ColorMine/ColorSpaces/ColorSpaces.cs
+++ ColorMine/ColorSpaces/ColorSpaces.cs
@@ -1,20 +1,38 @@
-﻿//Note: This is a generated file.
+﻿
+
+
+
+
+
+
+
+
+//Note: This is a generated file.
 using ColorMine.ColorSpaces.Conversions;
 
 namespace ColorMine.ColorSpaces
 {
+
 	public interface IRgb : IColorSpace
     {
+
 		double R { get; set; }
+
 		double G { get; set; }
+
 		double B { get; set; }
+
     }
 
     public class Rgb : ColorSpace, IRgb
     {
+
 		public double R { get; set; }
+
 		public double G { get; set; }
+
 		public double B { get; set; }
+
 
         public override void Initialize(IRgb color)
         {
@@ -27,18 +45,27 @@ namespace ColorMine.ColorSpaces
         }
     }
 
+
 	public interface IXyz : IColorSpace
     {
+
 		double X { get; set; }
+
 		double Y { get; set; }
+
 		double Z { get; set; }
+
     }
 
     public class Xyz : ColorSpace, IXyz
     {
+
 		public double X { get; set; }
+
 		public double Y { get; set; }
+
 		public double Z { get; set; }
+
 
         public override void Initialize(IRgb color)
         {
@@ -51,18 +78,27 @@ namespace ColorMine.ColorSpaces
         }
     }
 
+
 	public interface IHsl : IColorSpace
     {
+
 		double H { get; set; }
+
 		double S { get; set; }
+
 		double L { get; set; }
+
     }
 
     public class Hsl : ColorSpace, IHsl
     {
+
 		public double H { get; set; }
+
 		public double S { get; set; }
+
 		public double L { get; set; }
+
 
         public override void Initialize(IRgb color)
         {
@@ -75,18 +111,27 @@ namespace ColorMine.ColorSpaces
         }
     }
 
+
 	public interface ILab : IColorSpace
     {
+
 		double L { get; set; }
+
 		double A { get; set; }
+
 		double B { get; set; }
+
     }
 
     public class Lab : ColorSpace, ILab
     {
+
 		public double L { get; set; }
+
 		public double A { get; set; }
+
 		public double B { get; set; }
+
 
         public override void Initialize(IRgb color)
         {
@@ -99,18 +144,27 @@ namespace ColorMine.ColorSpaces
         }
     }
 
+
 	public interface ILch : IColorSpace
     {
+
 		double L { get; set; }
+
 		double C { get; set; }
+
 		double H { get; set; }
+
     }
 
     public class Lch : ColorSpace, ILch
     {
+
 		public double L { get; set; }
+
 		public double C { get; set; }
+
 		public double H { get; set; }
+
 
         public override void Initialize(IRgb color)
         {
@@ -123,18 +177,27 @@ namespace ColorMine.ColorSpaces
         }
     }
 
+
 	public interface ILuv : IColorSpace
     {
+
 		double L { get; set; }
+
 		double U { get; set; }
+
 		double V { get; set; }
+
     }
 
     public class Luv : ColorSpace, ILuv
     {
+
 		public double L { get; set; }
+
 		public double U { get; set; }
+
 		public double V { get; set; }
+
 
         public override void Initialize(IRgb color)
         {
@@ -147,18 +210,27 @@ namespace ColorMine.ColorSpaces
         }
     }
 
+
 	public interface IYxy : IColorSpace
     {
+
 		double Y1 { get; set; }
+
 		double X { get; set; }
+
 		double Y2 { get; set; }
+
     }
 
     public class Yxy : ColorSpace, IYxy
     {
+
 		public double Y1 { get; set; }
+
 		public double X { get; set; }
+
 		public double Y2 { get; set; }
+
 
         public override void Initialize(IRgb color)
         {
@@ -171,18 +243,27 @@ namespace ColorMine.ColorSpaces
         }
     }
 
+
 	public interface ICmy : IColorSpace
     {
+
 		double C { get; set; }
+
 		double M { get; set; }
+
 		double Y { get; set; }
+
     }
 
     public class Cmy : ColorSpace, ICmy
     {
+
 		public double C { get; set; }
+
 		public double M { get; set; }
+
 		public double Y { get; set; }
+
 
         public override void Initialize(IRgb color)
         {
@@ -195,20 +276,31 @@ namespace ColorMine.ColorSpaces
         }
     }
 
+
 	public interface ICmyk : IColorSpace
     {
+
 		double C { get; set; }
+
 		double M { get; set; }
+
 		double Y { get; set; }
+
 		double K { get; set; }
+
     }
 
     public class Cmyk : ColorSpace, ICmyk
     {
+
 		public double C { get; set; }
+
 		public double M { get; set; }
+
 		public double Y { get; set; }
+
 		public double K { get; set; }
+
 
         public override void Initialize(IRgb color)
         {
@@ -221,18 +313,27 @@ namespace ColorMine.ColorSpaces
         }
     }
 
+
 	public interface IHsv : IColorSpace
     {
+
 		double H { get; set; }
+
 		double S { get; set; }
+
 		double V { get; set; }
+
     }
 
     public class Hsv : ColorSpace, IHsv
     {
+
 		public double H { get; set; }
+
 		public double S { get; set; }
+
 		public double V { get; set; }
+
 
         public override void Initialize(IRgb color)
         {
@@ -245,18 +346,27 @@ namespace ColorMine.ColorSpaces
         }
     }
 
+
 	public interface IHsb : IColorSpace
     {
+
 		double H { get; set; }
+
 		double S { get; set; }
+
 		double B { get; set; }
+
     }
 
     public class Hsb : ColorSpace, IHsb
     {
+
 		public double H { get; set; }
+
 		public double S { get; set; }
+
 		public double B { get; set; }
+
 
         public override void Initialize(IRgb color)
         {
@@ -269,18 +379,27 @@ namespace ColorMine.ColorSpaces
         }
     }
 
+
 	public interface IHunterLab : IColorSpace
     {
+
 		double L { get; set; }
+
 		double A { get; set; }
+
 		double B { get; set; }
+
     }
 
     public class HunterLab : ColorSpace, IHunterLab
     {
+
 		public double L { get; set; }
+
 		public double A { get; set; }
+
 		public double B { get; set; }
+
 
         public override void Initialize(IRgb color)
         {
@@ -292,5 +411,6 @@ namespace ColorMine.ColorSpaces
             return HunterLabConverter.ToColor(this);
         }
     }
+
 
 }
